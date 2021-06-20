1e324dcd5f0ffa9eebebf6e28d6de701fb117661
hl <holgerlucas@gmx.net>
2021-06-20 15:04:09 +0200

    Kundenapp nav2

--- KundenApp/app/src/main/java/com/example/kundenapp/ui/home/BlankFragment.java
+++ KundenApp/app/src/main/java/com/example/kundenapp/ui/home/BlankFragment.java
@@ -12,21 +12,21 @@ import com.example.kundenapp.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TestnegativFragment#newInstance} factory method to
+ * Use the {@link BlankFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TestnegativFragment extends Fragment {
+public class BlankFragment extends Fragment {
 
-    /*// TODO: Rename parameter arguments, choose names that match
+    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
-    private String mParam2;*/
+    private String mParam2;
 
-    public TestnegativFragment() {
+    public BlankFragment() {
         // Required empty public constructor
     }
 
@@ -36,31 +36,31 @@ public class TestnegativFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_testnegativ.
+     * @return A new instance of fragment BlankFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TestnegativFragment newInstance(String param1, String param2) {
-        TestnegativFragment fragment = new TestnegativFragment();
-        //Bundle args = new Bundle();
-        //args.putString(ARG_PARAM1, param1);
-        //args.putString(ARG_PARAM2, param2);
-        //fragment.setArguments(args);
+    public static BlankFragment newInstance(String param1, String param2) {
+        BlankFragment fragment = new BlankFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
         return fragment;
     }
 
-    /*@Override
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-    }*/
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_testnegativ, container, false);
+        return inflater.inflate(R.layout.fragment_blank, container, false);
     }
 }
--- KundenApp/app/src/main/java/com/example/kundenapp/ui/home/HomeFragment.java
+++ KundenApp/app/src/main/java/com/example/kundenapp/ui/home/HomeFragment.java
@@ -9,9 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
 import com.example.kundenapp.R;
 import com.example.kundenapp.databinding.FragmentHomeBinding;
+import com.example.kundenapp.databinding.FragmentNotificationsBinding;
 
 public class HomeFragment extends Fragment {
 
@@ -37,8 +39,11 @@ public class HomeFragment extends Fragment {
         });
         return root;*/
 
-        binding = FragmentHomeBinding.inflate(getLayoutInflater());
-        return binding.getRoot();
+        //binding = FragmentHomeBinding.inflate(getLayoutInflater());
+        //return binding.getRoot();
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
     }
 
     public void onViewCreated(View view, Bundle savedInstanceState){
@@ -46,12 +51,16 @@ public class HomeFragment extends Fragment {
 
         binding.button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Fragment testnegativeFragment = new TestnegativFragment();
+                /*Fragment testnegativeFragment = new TestnegativFragment();
                 FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.nav_host_fragment_activity_main, testnegativeFragment);
                 fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
+                fragmentTransaction.commit();*/
+
+                Navigation.findNavController(v).navigate(R.id.action_navigation_home_to_fragment_testnegativ);
+
+
             }
         });
     }

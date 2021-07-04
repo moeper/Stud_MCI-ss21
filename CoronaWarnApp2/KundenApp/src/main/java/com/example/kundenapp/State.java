5ea794c705abe111a2a835e3a2a449e29b350ccb
hl <holgerlucas@gmx.net>
2021-07-05 00:08:49 +0200

    kundenapp checkin zeit angeben

--- CoronaWarnApp2/KundenApp/src/main/java/com/example/kundenapp/State.java
+++ CoronaWarnApp2/KundenApp/src/main/java/com/example/kundenapp/State.java
@@ -3,9 +3,11 @@ package com.example.kundenapp;
 public class State {
         public boolean checkin_active;
         public Integer checkin_duration;
+        public boolean checkin_isNew;
 
         public State(){
                 checkin_active = false;
-                checkin_duration = 30;
+                checkin_duration = 20;
+                checkin_isNew = false;
         }
 }
